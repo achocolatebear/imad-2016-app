9de36e6bb54606f2ef59174f4a16c1ad2313eca8
achocolatebear <harshamadhavan@gmail.com>
2016-10-15 10:14:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ app.get('/article-one', function(req, res){
 });
 
 app.get('/article-two', function(req, res){
- res.sendFile('Article 2 will be served shortly');
+ res.send('Article 2 will be served shortly');
 });
 
 app.get('/article-three', function(req, res){
- res.sendFile('Article 3 will be served shortly');
+ res.send('Article 3 will be served shortly');
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.send(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
