0d5b7d09a7dba297986b03059754be381c877aa1
achocolatebear <harshamadhavan@gmail.com>
2016-10-15 10:11:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('article-one', function(req, res){
+ res.sendFile('Article One will be served shortly');
+});
+
+app.get('article-two', function(req, res){
+ res.sendFile('Article 2 will be served shortly');
+});
+
+app.get('article-three', function(req, res){
+ res.sendFile('Article 3 will be served shortly');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
